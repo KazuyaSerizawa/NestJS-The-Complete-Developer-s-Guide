835937390f049794d98ca24e8f1af931936ccf59
KazuyaSerizawa <kserizawa081@gmail.com>
2021-11-29 22:22:43 +0900

    feat: 🎸 section 11

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -9,12 +9,16 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from "@nestjs/common";
+import { AuthGuard } from "src/auth.guard";
 import { Serialize } from "src/interceptors/serialize-interceptor";
 import { AuthService } from "./auth.service";
+import { CurrentUser } from "./decorators/current-user.decorator";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UserDto } from "./dtos/user.dto";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
 @Controller("auth")
@@ -25,24 +29,27 @@ export class UsersController {
     private authService: AuthService
   ) {}
 
-  @Get("colors/:color")
-  setColor(@Param("color") color: string, @Session() session: any) {
-    session.color = color;
+  @Get("whoami")
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
-
-  @Get("/colors")
-  getColor(@Session() session: any) {
-    return session.color;
+  @Post("/signout")
+  async signOut(@Session() session: any) {
+    session.userId = null;
   }
-
   @Post("/signup")
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post("/signin")
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Get("/:id")
