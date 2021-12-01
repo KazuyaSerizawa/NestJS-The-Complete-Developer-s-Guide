bf911647f649f679e6716c3766f3409e0576c1fe
KazuyaSerizawa <kserizawa081@gmail.com>
2021-12-01 21:15:26 +0900

    feat: 🎸 part 90

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
 it("can create an instace of auth service", async () => {
   // create a fake copy of the users serice
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
   const module = await Test.createTestingModule({
     providers: [
