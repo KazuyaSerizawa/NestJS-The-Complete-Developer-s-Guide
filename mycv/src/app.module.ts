eace86edeb6621e69557253f077433e346555350
KazuyaSerizawa <kserizawa081@gmail.com>
2021-12-26 19:40:52 +0900

    feat: 🎸 part 108

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -1,24 +1,44 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
+import { MiddlewareConsumer, Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { ReportsModule } from "./reports/reports.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/user.entity";
+import { Report } from "./reports/report.entity";
+const cookieSession = require("cookie-session");
 
 @Module({
   imports: [
     UsersModule,
     ReportsModule,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
+      type: "sqlite",
+      database: "db.sqlite",
       entities: [User, Report],
       synchronize: true,
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ["aaassfaf"],
+        })
+      )
+      .forRoutes("*");
+  }
+}
