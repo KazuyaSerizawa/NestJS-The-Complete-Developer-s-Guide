29bd6793a9644a547efc907b42fa0abc2c49edb0
KazuyaSerizawa <kserizawa081@gmail.com>
2021-12-28 18:28:43 +0900

    feat: 🎸 part 113

--- mycv/src/app.module.ts
+++ mycv/src/app.module.ts
@@ -7,18 +7,34 @@ import { ReportsModule } from "./reports/reports.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/user.entity";
 import { Report } from "./reports/report.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 const cookieSession = require("cookie-session");
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     UsersModule,
     ReportsModule,
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "db.sqlite",
-      entities: [User, Report],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: "sqlite",
+          database: config.get("DB_NAME"),
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
     }),
+    // TypeOrmModule.forRoot({
+    //   type: "sqlite",
+    //   database: "db.sqlite",
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [
