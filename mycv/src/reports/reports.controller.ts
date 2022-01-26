3563f610fefb16eb46d1825910a63c32b3317b0f
KazuyaSerizawa <kserizawa081@gmail.com>
2022-01-26 21:51:49 +0900

    feat: 🎸 127

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -7,6 +7,8 @@ import {
   UseGuards,
 } from "@nestjs/common";
 import { AuthGuard } from "src/guards/auth.guard";
+import { CurrentUser } from "src/users/decorators/current-user.decorator";
+import { User } from "src/users/user.entity";
 import { ApproveReportDto } from "./dtos/approve-report.dto";
 import { CreateReportDto } from "./dtos/create-report.dto";
 import { ReportsService } from "./reports.service";
@@ -16,8 +18,8 @@ export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
   @Patch(":id")
   approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {}
