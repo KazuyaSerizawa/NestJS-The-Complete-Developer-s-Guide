bab2b3e8c50ebd910ae9ab36f473d19d1d02d9f9
KazuyaSerizawa <kserizawa081@gmail.com>
2022-01-28 23:51:11 +0900

    feat: 🎸 129

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -7,10 +7,12 @@ import {
   UseGuards,
 } from "@nestjs/common";
 import { AuthGuard } from "src/guards/auth.guard";
+import { Serialize } from "src/interceptors/serialize-interceptor";
 import { CurrentUser } from "src/users/decorators/current-user.decorator";
 import { User } from "src/users/user.entity";
 import { ApproveReportDto } from "./dtos/approve-report.dto";
 import { CreateReportDto } from "./dtos/create-report.dto";
+import { ReportDto } from "./dtos/report.dto";
 import { ReportsService } from "./reports.service";
 
 @Controller("reports")
@@ -18,6 +20,7 @@ export class ReportsController {
   constructor(private reportsService: ReportsService) {}
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
