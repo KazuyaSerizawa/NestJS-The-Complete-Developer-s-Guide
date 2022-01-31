dc344aca28733e16fd1f0231c1b6329557a0558b
KazuyaSerizawa <kserizawa081@gmail.com>
2022-01-31 21:16:56 +0900

    feat: 🎸 134

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -6,6 +6,7 @@ import {
   Post,
   UseGuards,
 } from "@nestjs/common";
+import { AdminGuard } from "src/guards/admin.guard";
 import { AuthGuard } from "src/guards/auth.guard";
 import { Serialize } from "src/interceptors/serialize-interceptor";
 import { CurrentUser } from "src/users/decorators/current-user.decorator";
@@ -25,6 +26,7 @@ export class ReportsController {
     return this.reportsService.create(body, user);
   }
   @Patch(":id")
+  @UseGuards(AdminGuard)
   approveReport(@Param("id") id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
